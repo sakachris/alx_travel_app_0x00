9f860c36fbf951412fb3fd3980f0598618002a18
sakachris <sakachris90@gmail.com>
2025-05-30 08:34:59 +0300

    models

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,14 @@
 # serializers.py
+
+from rest_framework import serializers
+from .models import Property, Booking
+
+class PropertySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
